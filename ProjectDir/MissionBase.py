6186ca852d9a8ec5bf63fbc6834de53c1e5ec7aa
ssaltzen <ssaltzen@uci.edu>
2017-05-20 13:31:37 -0700

    Setup most of the mission
    Still a bit buggy but this'll get us through most of it I hope

--- ProjectDir/MissionBase.py
+++ ProjectDir/MissionBase.py
@@ -2,85 +2,177 @@ import MalmoPython
 import os
 import sys
 import time
-from XMLgen import generateXML
-import AgentModule
+import json
+import random
+from XMLGen import generateXML
 
-XML_DIMENSIONS_TUPLE = (25,25,25)
-
-worldXML = generateXML(XML_DIMENSIONS_TUPLE)
+worldXML = generateXML((25,25,25)) #maze size x y z
 
 #--------SET UP MALMO INTERFACE-------
-def startMission(agent_host, xml):
-    my_mission = MalmoPython.MissionSpec(xml, True)
-    my_mission_record = MalmoPython.MissionRecordSpec()
-    agent_host.startMission( my_mission, my_mission_record )
-    world_state = agent_host.peekWorldState()
-    while not world_state.has_mission_begun:
+def startMission(agentHost, xml):
+    mission = MalmoPython.MissionSpec(xml, True)
+    missionRecord = MalmoPython.MissionRecordSpec() # Need to set up
+    agentHost.startMission( mission, missionRecord )
+    worldState = agentHost.peekWorldState()
+    while not worldState.has_mission_begun:
         time.sleep(0.1)
-        world_state = agent_host.peekWorldState()
-        for error in world_state.errors:
+        worldState = agentHost.peekWorldState()
+        for error in worldState.errors:
             print "Error:",error.text
-        if len(world_state.errors) > 0:
+        if len(worldState.errors) > 0:
             exit(1)
 #------------------------------------------
 
+
+#-----------AGENENT RUN-------------------- Suggest moving this to agent
+actions = ["movenorth 1", "movesouth 1", "movewest 1", "moveeast 1"]
+
+def run(agentHost):
+    """run the agent on the world"""
+
+    totalReward = 0
+    currentRewards = 0
+
+    previousState = None #may need?
+    previousAction = None
+
+    # wait for a valid observation
+    worldState = agentHost.peekWorldState()
+    while worldState.is_mission_running and all(e.text == '{}' for e in worldState.observations):
+        worldState = agentHost.peekWorldState()
+
+    if not worldState.is_mission_running:
+        return 0  # mission already ended
+
+    obs = json.loads(worldState.observations[-1].text)
+    print obs
+    prevX = obs[u'XPos']
+    prevZ = obs[u'ZPos']
+    print 'Initial position:', prevX, ',', prevZ
+
+    #totalReward += agentHost.sendCommand() #should be total reward plus whatever the last action got?  Maybe want to do rewards based on item collected not inventory
+
+    #Take actions ----
+
+    time.sleep(1)
+    agentHost.sendCommand('movenorth 1')
+    print 'move'
+    time.sleep(1)
+
+    print 'Waiting for data...',
+    while worldState.is_mission_running:
+        while True:
+            worldState = agentHost.peekWorldState()
+            if not worldState.is_mission_running:
+                print 'mission ended.'
+                break
+
+        worldState = agentHost.getWorldState()
+        for err in worldState.errors:
+            print err
+
+        current_r = sum(r.getValue() for r in worldState.rewards)
+
+
+#-----------AGENENT RUN--------------------
+
 #----------OUR MISSION CODE----------------
-def runOurMission(agent_host):
-    agent_host.sendCommand('tp 1 7 1')
-    #agent_host.sendCommand('setworldspawn 1 6 1')
-    '''
+'''
+def runOurMission(agentHost):
+    TESTING STUFF
+    #agentHost.sendCommand('tp 0 227 0')
+    time.sleep(1)
+    loc = 0
+    for i in range(10):
+
+        agentHost.sendCommand('movenorth 1')
+        print 'move'
+        time.sleep(1)
+        """
+        agentHost.sendCommand('tp ' + str(loc) + ' 227 ' +str(loc))
+        loc+=1
+        print loc, " moved\n"
+        time.sleep(1)
+        """
+
+    #agentHost.sendCommand('setworldspawn 1 6 1')
+    
     for x in xrange(3):
         for z in xrange(3):
             teleport_x = x * 2 + 1
             teleport_z = z * 2 + 1
             tp_command = "tp " + str(teleport_x)+ " 4 " + str(teleport_z) # command string is x y z, appears this map uses 4 as base
             print "Sending command: " + tp_command
-            agent_host.sendCommand(tp_command)
+            agentHost.sendCommand(tp_command)
             time.sleep(2) # TIME BETWEEN MOVES
-    '''
-    agent_host.sendCommand("quit")
+    
+    agentHost.sendCommand("quit")
+'''
 # ------------------------------------------
 
 #----CONNECT/SET UP AGENT AND RUN MISSION-----
 if __name__ == "__main__":
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-    agent_host = MalmoPython.AgentHost()
+    agentHost = MalmoPython.AgentHost()
 
     try:
-        agent_host.parse( sys.argv )
+        agentHost.parse( sys.argv )
     except RuntimeError as e:
         print 'ERROR:',e
-        print agent_host.getUsage()
+        print agentHost.getUsage()
         exit(1)
-    if agent_host.receivedArgument("help"):
-        print agent_host.getUsage()
+    if agentHost.receivedArgument("help"):
+        print agentHost.getUsage()
         exit(0)
 
-    startMission(agent_host, worldXML)
-    world_state = agent_host.peekWorldState()
+    startMission(agentHost, worldXML)
+    worldState = agentHost.peekWorldState()
     #-----------------------------------------
 
 
     #----------------RUN MISSION--------------
-    #Currently is teleport around with no purpose
+    #Currently is moving north around with no purpose
+    #runOurMission(agentHost)
+    maxRetry = 3
+    maxMoves = 3
+    finalRewards = []
+    for i in range(maxMoves):
+        print "Mission starting",
+        world_state = agentHost.getWorldState()
+        while not world_state.has_mission_begun:
+            print "."
+            time.sleep(0.1)
+            world_state = agentHost.getWorldState()
+            for error in world_state.errors:
+                print "Error:", error.text
+        print
 
-    agent = AgentModule.Agent()
-    agent.ourAgent = agent_host
-    agent.makePath(1000)
-    # runOurMission(agent_host)
+        # -- run the agent in the world -- #
+        run(agentHost) #save rewards at some poitn
 
+        print 'Final reward:',  finalRewards
+        #finalRewards += what ever last rewards were
+
+        # -- clean up -- #
+        time.sleep(0.5)  # (let the Mod reset)
+
+    print "Done."
+
+    print
+    print "Final rewards for all ", maxMoves, ' runs:'
+    print finalRewards
 
     #-------------------------------------------
 
     #------END MISSION AND CHECK REWARDS--------
-
-    while world_state.is_mission_running:
-        world_state = agent_host.peekWorldState()
+    """
+    while worldState.is_mission_running:
+        worldState = agentHost.peekWorldState()
 
     print "Mission over."
 
     '''
-    world_state = agent_host.getWorldState()
+    worldState = agentHost.getWorldState()
     if Mission Ran :
         Calculate Final Rewards
     if not sucessful:
@@ -90,4 +182,4 @@ if __name__ == "__main__":
 
     print "Test successful"
     exit(0)
-
+    """
--- ProjectDir/XMLgen.py
+++ ProjectDir/XMLgen.py
@@ -1,68 +1,151 @@
 import MazeGen
+import MalmoPython
 
-def generateXML(mazeSize):
-    '''Takes a three tuple or iterabele'''
-    WIDTH = 860
-    HEIGHT = 480
+#Stuff to do.
+""""
+Clean up rewards
+set view point behind
+change skin? 
+Timer?
+change sleep times
 
+"""
+
+HEIGHT = 226
+itemRewards = {"apple":25, "carrot":50, "emerald":70, "diamond":100}
+
+def generateXML(mazeSize ):
+    global HEIGHT
+
+    #USE THIS TO SET UP WITH MODIFICATIONS
+    initialXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+  <About>
+    <Summary>Lars 175</Summary>
+  </About>
+  
+  <ModSettings>
+    <MsPerTick>1</MsPerTick>
+  </ModSettings>
+
+  <ServerSection>
+      <ServerInitialConditions>
+            <Time>
+                <StartTime>6000</StartTime>
+                <AllowPassageOfTime>false</AllowPassageOfTime>
+            </Time>
+            <Weather>clear</Weather>
+            <AllowSpawning>false</AllowSpawning>
+      </ServerInitialConditions>
+    <ServerHandlers>
+      <FlatWorldGenerator generatorString="3;7,220*1,5*3,2;3;,biome_1"/>
+      <DrawingDecorator>
+        <!-- coordinates for cuboid are inclusive -->
+        <DrawCuboid x1="-2" y1="46" z1="-2" x2="7" y2="50" z2="18" type="air" />            <!-- limits of our arena -->
+      </DrawingDecorator>
+      <ServerQuitFromTimeUp timeLimitMs="1000000"/>
+      <ServerQuitWhenAnyAgentFinishes/>
+    </ServerHandlers>
+  </ServerSection>
+
+  <AgentSection mode="Survival">
+    <Name>Lars</Name>
+    <AgentStart>
+      <Placement x="0" y="227" z="0" pitch="30" yaw="0"/>
+    </AgentStart>
+    <AgentHandlers>
+      <ObservationFromFullStats/>
+      <VideoProducer want_depth="false">
+          <Width>640</Width>
+          <Height>480</Height>
+      </VideoProducer>
+      <DiscreteMovementCommands>
+          <ModifierList type="deny-list">
+            <command>attack</command>
+          </ModifierList>
+      </DiscreteMovementCommands>
+      <RewardForTouchingBlockType>
+        <Block reward="-100.0" type="lava" behaviour="onceOnly"/>
+      </RewardForTouchingBlockType>
+      <RewardForSendingCommand reward="-1"/>
+      <AgentQuitFromTouchingBlockType>
+          <Block type="lava" />
+      </AgentQuitFromTouchingBlockType>
+    </AgentHandlers>
+  </AgentSection>
+
+</Mission>'''
+
+
+
+
+
+
+    #Set up Misscion class
+    missionSpecs = MalmoPython.MissionSpec(initialXML,True)
+
+    #Describe Mission
+    missionSpecs.setSummary("LARS Project 175")
+
+    #/setworldspawn 0 227 0
+
+    #Timer??
+    #missionSpecs.timeLimitInSeconds(100)
+
+    #Day Light parameteers, defualt 1000, morning not allowing time to pass
+    missionSpecs.setTimeOfDay(13200 ,False)
+
+    #For Future use, set a entrance or exit state to be recorded
+    missionSpecs.startAt(0, 227, 0) #- float x, float y, float z, float tolerance
     maze = MazeGen.genMaze(mazeSize)
     mazeValue = maze.maze
 
-    def genItems():
-        items = ""
 
-        items += '<DrawBlock x="' + str(0) + '" y="' + str(4) + '" ' + 'z="' + str(0) + '" type="' + str('dirt') + '"/>' #Start block
+    #Draw world in XML
+    missionSpecs.drawBlock(-1,227,-1,'glowstone')#Start block
 
-        for xVal in range(len(mazeValue)):
-            x = xVal
-            for zVal in range(len(mazeValue[xVal])):
-                z = zVal
-                for yVal in range(len(mazeValue[xVal][zVal])):
-                    y = yVal
-                    items += '<DrawBlock x="' + str(x) + '" y="' + str(y + 3) + '" '+ 'z="' + str(z) + '" type="' + str(mazeValue[x][z][y]) +'"/>'
-                    #items += '<DrawItem x="' + str(x) + '" y="' + str(y + 3) + '" '+ 'z="' + str(z+6) + '" type="apple"/>'
+    missionSpecs.drawBlock(-1, 227, mazeSize[2]+1, 'glowstone')  # Start block
+    missionSpecs.drawBlock(mazeSize[0]+1, 227,mazeSize[2]+1, 'glowstone')  # Start block
+    missionSpecs.drawBlock(mazeSize[0]+1, 227, -1, 'glowstone')  # Start block
 
-        return items
+    missionSpecs.drawCuboid(-10, HEIGHT, -10, mazeSize[0]+10, mazeSize[1]+HEIGHT+1, mazeSize[2]+10, 'obsidian')
+    missionSpecs.drawCuboid(-9, HEIGHT, -9, mazeSize[0]+9, mazeSize[1]+HEIGHT, mazeSize[2]+9, 'air')
+    missionSpecs.drawCuboid(-9, HEIGHT, -9, mazeSize[0] + 9, HEIGHT, mazeSize[2] + 9, 'lava')
 
-    worldXML = '''<?xml version="1.0" encoding="UTF-8" ?>
-        <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <About>
-                <Summary>Testing Base -- CHANGE THIS</Summary>
-            </About>
-            <ServerSection>
-                <ServerInitialConditions>
-                    <Time>
-                        <StartTime>1000</StartTime>
-                        <AllowPassageOfTime>false</AllowPassageOfTime>
-                    </Time>
-                    <Weather>clear</Weather>
-                </ServerInitialConditions>
-                <ServerHandlers>
-                    <FlatWorldGenerator generatorString="3;;1;" forceReset="true" destroyAfterUse="true"/>
-                    <DrawingDecorator>''' + genItems() + '''</DrawingDecorator>
-                    <ServerQuitWhenAnyAgentFinishes />
-                </ServerHandlers>
-            </ServerSection>
-            <AgentSection mode="Survival">
-                <Name>ChickenChicken</Name>
-                <AgentStart>
-                    <Placement x="0" y="4" z="0"/>
-                    <Inventory/>
-                </AgentStart>
-                <AgentHandlers>
-                    <ContinuousMovementCommands turnSpeedDegs="360"/>
-                    <ObservationFromFullInventory/>
-                    <AbsoluteMovementCommands/>
-                    <MissionQuitCommands/>
-                    <RewardForCollectingItem>
-                        <Item type="emerald" reward="1"/>
-                    </RewardForCollectingItem>
-                    <VideoProducer>
-                        <Width>''' + str(WIDTH) + '''</Width>
-                        <Height>''' + str(HEIGHT) + '''</Height>
-                    </VideoProducer>
-                </AgentHandlers>
-            </AgentSection>
-        </Mission>'''
 
-    return worldXML
+    #Draw Maze/Items
+    for xVal in range(len(mazeValue)):
+
+        x = xVal
+        for zVal in range(len(mazeValue[xVal])):
+            z = zVal
+            for yVal in range(len(mazeValue[xVal][zVal])):
+                y = yVal
+                missionSpecs.drawBlock(x,(y + HEIGHT),z, str(mazeValue[x][z][y]))
+                if str(mazeValue[x][z][y]) == "lapis_block":
+                    missionSpecs.drawItem(x, (y + HEIGHT + 2), z, "apple")
+
+    #Observations
+    missionSpecs.observeFullInventory()  #Full item inventory of the player included in the observations
+    missionSpecs.observeRecentCommands() # list of commands acted upon since the last timestep
+    #missionSpecs.observeDistance# (float x, float y, float z, const std::string & name) For use once an end in place
+
+
+    #Rewards
+    #rewardForReachingPosition(float x, float y, float, z, float amount, float tolerance) # to be included when end is implemented
+
+
+    #Settings
+    missionSpecs.allowAllDiscreteMovementCommands()
+    #missionSpecs.allowAllAbsoluteMovementCommands()
+    missionSpecs.setViewpoint( 1 )
+    missionSpecs.allowAllInventoryCommands()
+
+    return missionSpecs.getAsXML(True)
+
+print generateXML((25,25,25))
+
+
+
+
